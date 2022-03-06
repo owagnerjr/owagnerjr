55cd5c856fc433fb96f05ec3d4c6823acc1f1351
owagnerjr <101050460+owagnerjr@users.noreply.github.com>
2022-03-06 11:26:37 -0300

    Add files via upload

---
+++ Vagao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaEncadeada
+{
+     public class Vagao
+    {
+        public int Id { get; set; }
+        public string Carga { get; set; }
+        public int PesoEmToneladas { get; set; }
+        public Vagao Atras { get; set; }
+    }
+}
