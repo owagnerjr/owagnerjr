5d56668aa971f1a052f19d2695ad96411e4e3113
owagnerjr <101050460+owagnerjr@users.noreply.github.com>
2022-03-06 11:38:43 -0300

    Arquivos feitos no Computador

---
+++ ListaEncadeada-main/ListaEncadeada/GerenteDoTrem.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaEncadeada
+{
+     public class GerenteDoTrem
+    {
+        public Vagao Locomotiva { get; set; }
+
+        #region Create
+        public void InserirVagao(Vagao novoVagao)
+        {
+            if (Locomotiva == null)
+            {
+                Locomotiva = novoVagao;
+            }
+            else
+            {
+                var ultimoVagao = GetUltimoVagao();
+                ultimoVagao.Atras = novoVagao;
+            }
+        }
+
+        private Vagao GetUltimoVagao()
+        {
+            var tempVagao = Locomotiva;
+            while (tempVagao.Atras != null)
+            {
+                tempVagao = tempVagao.Atras;
+            }
+            return tempVagao;
+        }
+        #endregion
+
+        #region Read
+        public List<Vagao> GetVagoes()
+        {
+            var trem = new List<Vagao>();
+            var tempVagao = Locomotiva;
+            while (tempVagao != null)
+            {
+                trem.Add(tempVagao);
+                tempVagao = tempVagao.Atras;
+            }
+            return trem;
+        }
+        #endregion
+        #region Update
+        public void AtualizarVagao(Vagao vagao)
+        {
+            var tempVagao = Locomotiva;
+            while (tempVagao != null)
+            {
+                if (vagao.Id == tempVagao.Id)
+                {
+                    tempVagao.Carga = vagao.Carga;
+                    tempVagao.PesoEmToneladas = vagao.PesoEmToneladas;
+                    return;
+                }
+                tempVagao = tempVagao.Atras;
+            }
+        }
+        #endregion
+
+        #region Delete
+        public void DeletarVagao(int vagaoId)
+        {
+            var tempVagao = Locomotiva;
+            if (Locomotiva.Id == vagaoId)
+            {
+                Locomotiva = Locomotiva.Atras;
+            }
+            else
+            {
+                while (tempVagao != null)
+                {
+                    if (vagaoId == tempVagao.Atras.Id)
+                    {
+                        tempVagao.Atras = tempVagao.Atras.Atras;
+                        return;
+                    }
+                    tempVagao = tempVagao.Atras;
+                }
+            }
+
+        }
+        #endregion
+    }
+}
---
+++ ListaEncadeada-main/ListaEncadeada/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ListaEncadeada;
+
+static void Main(string[] args)
+{
+    var gerenteDoTrem = new GerenteDoTrem();
+
+    InserirVagoesNoTrem(gerenteDoTrem);
+    ImprimeTodosOsVagoes(gerenteDoTrem);
+    AtualizaVagao(gerenteDoTrem);
+    ImprimeTodosOsVagoes(gerenteDoTrem);
+    DeletaVagao(gerenteDoTrem);
+    ImprimeTodosOsVagoes(gerenteDoTrem);
+}
+
+ static void InserirVagoesNoTrem(GerenteDoTrem gerenteDoTrem)
+{
+    var vagao01 = new Vagao() { Id = 123, Carga = "Pedras", PesoEmToneladas = 15 };
+    var vagao02 = new Vagao() { Id = 234, Carga = "Tabuas", PesoEmToneladas = 1 };
+    var vagao03 = new Vagao() { Id = 345, Carga = "imas", PesoEmToneladas = 9 };
+    var vagao04 = new Vagao() { Id = 456, Carga = "Cadeiras", PesoEmToneladas = 4 };
+    var vagao05 = new Vagao() { Id = 567, Carga = "portas", PesoEmToneladas = 5 };
+
+    gerenteDoTrem.InserirVagao(vagao03);
+    gerenteDoTrem.InserirVagao(vagao01);
+    gerenteDoTrem.InserirVagao(vagao04);
+    gerenteDoTrem.InserirVagao(vagao05);
+    gerenteDoTrem.InserirVagao(vagao02);
+}
+
+ static void ImprimeTodosOsVagoes(GerenteDoTrem gerenteDoTrem)
+{
+    Console.WriteLine($"Escrevendo todos os vagoes do trem");
+    foreach (var item in gerenteDoTrem.GetVagoes())
+    {
+        Console.WriteLine($"ID {item.Id} - Carga {item.Carga} - Peso {item.PesoEmToneladas}");
+    }
+    Console.WriteLine();
+}
+
+ static void AtualizaVagao(GerenteDoTrem gerenteDoTrem)
+{
+    var Vagao = new Vagao() { Id = 456, Carga = "Luzes", PesoEmToneladas = 7 };
+    gerenteDoTrem.AtualizarVagao(Vagao);
+}
+
+ static void DeletaVagao(GerenteDoTrem gerenteDoTrem)
+{
+    gerenteDoTrem.DeletarVagao(345);
+}
+    
+
